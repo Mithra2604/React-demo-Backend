a59f19e5ff9bcb375fc323b1f19503d22c8731a7
mi@26041988 <mformithra@gmail.com>
2024-05-31 11:53:56 +0530

    first commit

---
+++ sring-master/src/test/java/com/example/SpringBoot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class chApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
